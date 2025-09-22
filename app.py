392de1f8813cfac741f48bd16f7a9ace43a3d8ff
Стабровская Елизавета <liza.stabrovskaya2894@gmail.com>
2025-09-22 23:24:04 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,12 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<! doctype html>" 
         "<html>"
         "   <body>"  
         "       <h1>web-сервер на flask</hl>" 
+                <p><a href ="/author">author</a></p>
         "   </body>" 
         "</html>"""
 
+@app.route("/author")
+def author():
+    name = 'Стабровская Елизавета Евгеньевна'
+    group= 'ФБИ-33'
+    faculty = 'ФБ'
 
+    return """<! doctype html>
+        <html>
+            <body>
+                <p>Студент: """+name+"""</p>
+                <p>Группа: """+group+"""</p>
+                <p>Факультет: """+faculty+"""</p>
+                <a href ="/web">web</a>
+            </body>
+        </html>"""
