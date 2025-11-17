b98b48740dc812b6620ec8b8f821c1a0e3410d76
Стабровская Елизавета <liza.stabrovskaya2894@gmail.com>
2025-11-17 23:22:08 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,19 +1,18 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request
+import psycopg2
 
 lab5 = Blueprint('lab5', __name__)
 
+
 @lab5.route("/")
 def main():
-    login = session.get('login', 'Anonymous')
-    return render_template('lab5/lab5.html', login=login)
+    username = session.get('username', 'anonymous')
+    return render_template('lab5/lab5.html', username=username)
 
 @lab5.route("/lab5/login")
 def login():
-    return "Страница входа"
+    return render_template('lab5/login.html')  
 
-@lab5.route("/lab5/register")
-def register():
-    return "Страница регистрации"
 
 @lab5.route("/lab5/list")
 def list_articles():
@@ -22,3 +21,39 @@ def list_articles():
 @lab5.route("/lab5/create")
 def create_article():
     return "Создать статью"
+
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+    
+    try:
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='liza_stabrovskaya_knowledge_base',
+            user='liza_stabrovskaya_knowledge_base',
+            password='555'
+        )
+        cur = conn.cursor()  
+
+        cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
+        if cur.fetchone():
+            cur.close()
+            conn.close()
+            return render_template('lab5/register.html', error='Такой пользователь уже существует')
+        
+        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password))  
+        conn.commit()  
+        cur.close()
+        conn.close()
+        return render_template('lab5/success.html', login=login)
+    
+    except Exception as e:
+        return render_template('lab5/register.html', error=f'Ошибка БД: {str(e)}')
