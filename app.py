3ee56cd0e87ea5de3dc660e389f9d5ba90760c8b
Стабровская Елизавета <liza.stabrovskaya2894@gmail.com>
2025-12-14 11:02:34 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 import datetime
 import os
-import random  # Добавлен импорт random
+import random  
 
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.route('/start')
 def start():
