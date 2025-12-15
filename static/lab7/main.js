410387e48f76558bb9ec6bb3be22f059827eca27
Стабровская Елизавета <liza.stabrovskaya2894@gmail.com>
2025-12-16 00:44:46 +0700

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -20,6 +20,9 @@ function fillFilmList() {
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                editFilm(i);
+            };
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -71,17 +74,33 @@ function addFilm() {
     showModal();
 }
 
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title_ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
+    });
+}
+
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title_ru').value,
         year: document.getElementById('year').value,
         description: document.getElementById('description').value
     };
-
-    const url = '/lab7/rest-api/films/';
-    const method = 'POST';
-
+    
+    const url = id === '' ? '/lab7/rest-api/films/' : `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
+    
     fetch(url, {
         method: method,
         headers: {'Content-Type': 'application/json'},
