533e07cab1e6e7b7792f016f117d72e0bf25e0dd
Лиза Стабровская <liza.stabrovskaya2894@gmail.com>
2025-12-26 15:43:43 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, url_for, request, redirect, render_template, abort, make_response, send_file, Response, abort
+from flask import Flask, url_for, request, redirect, abort, render_template
+import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from db.models import users
+
+# Импорт db из пакета db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -7,14 +15,29 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
-import datetime
-import os
-import random  
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+
+    db_name = 'liza_stabrovskaya_orm'
+    db_user = 'liza_stabrovskaya_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "liza_stabrovskaya_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -27,7 +50,7 @@ app.register_blueprint(lab8)
 
 @app.route('/')
 @app.route('/index')
-@app.route('/start')  # Все три адреса ведут на главное меню
+@app.route('/start')  
 def index():
     return render_template('main_menu.html')
 
